dfd1dedcdbaf55ef25743b50b5021ea0a5cc72fc
Aaron Arenas Martinez <44418304+UTLCDA@users.noreply.github.com>
2025-04-14 07:32:12 -0600

    Validaciones de campos al momento de guardar

--- SiniestroCentralizadoGtoApi/Modelos/Reporte.cs
+++ SiniestroCentralizadoGtoApi/Modelos/Reporte.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -8,43 +9,43 @@ namespace SiniestroCentralizadoGtoApi.Modelos;
 public partial class Reporte
 {
     public int Id { get; set; }
-
-    public string PolizaId { get; set; }
-
-    public int? ContratanteId { get; set; }
-
-    public int? VehiculoId { get; set; }
-
-    public int? AjustadorId { get; set; }
-
+    [Required(ErrorMessage = "El campo Id Sucursal es obligatorio.")]
     public int? SucursalId { get; set; }
-
+    [Required(ErrorMessage = "El campo Id poliza es obligatorio")]
+    public string PolizaId { get; set; }
+    [Required(ErrorMessage = "El campo Id Contratante es obligatorio")]
+    public int? ContratanteId { get; set; }
+    [Required(ErrorMessage = "El campo Vehiculo es obligatorio")]
+    public int? VehiculoId { get; set; }
+    [Required(ErrorMessage = "El campo Fecha siniestro es obligatorio")]
     public DateTime FechaSiniestro { get; set; }
-
+    [Required(ErrorMessage = "El campo Lugar Siniestro Dirección es obligatorio")]
     public string? LugarSiniestroDireccion { get; set; }
-
+    [Required(ErrorMessage = "El campo Lugar Siniestro Coordenadas es obligatorio")]
     public string? LugarSiniestroCoordenadas { get; set; }
-
+    [Required(ErrorMessage = "El campo Taller es obligatorio")]
     public string? Taller { get; set; }
-
+    [Required(ErrorMessage = "El campo Responsable es obligatorio")]
     public string? Responsable { get; set; }
-
+    [Required(ErrorMessage = "El campo Número de Siniestro es obligatorio")]
     public int? NumSiniestro { get; set; }
-
+    [Required(ErrorMessage = "El campo Folio Reporte es obligatorio")]
     public string? FolioReporte { get; set; }
-
+    [Required(ErrorMessage = "El campo Telefono Propietario es obligatorio")]
     public string? TelefonoPropietrio { get; set; }
-
+    [Required(ErrorMessage = "El campo Nombre Asegurado es obligatorio")]
     public string? NombreAsegurado { get; set; }
-
+    [Required(ErrorMessage = "El campo Nombre propietario es obligatorio")]
     public string? NombrePropietario { get; set; }
 
     public bool AplicaDeducible { get; set; }
 
     public decimal? PorcentajeDeducible { get; set; }
-
+    [Required(ErrorMessage = "El Descripción siniestro Póliza es obligatorio")]
     public string? DescripcionSiniestro { get; set; }
 
+    public int? AjustadorId { get; set; }
+    [Required(ErrorMessage = "El campo Observaciones Ajustador es obligatorio")]
     public string? ObservacionesAjustador { get; set; }
     [NotMapped]
     public bool EsCorrecto { get; set; }
